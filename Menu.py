5d7515e21a428f61f8f7b785db5d3c1aa8a8fd9e
“siddhikapadi” <siddhikapdi@gmail.com>
2024-09-23 10:17:49 +0000

    Menu code of beverages branch.

--- Menu.py
+++ Menu.py
@@ -2,4 +2,8 @@ hungry=input ("Are you hungry?")
 if hungry=="yes":
     print("eat samosa")
 else:
+    thirsty=input ("Are you thirsty?")
+if thirsty=="yes":
+    print("Have some water")
+else:    
     print("Do your homework.")
